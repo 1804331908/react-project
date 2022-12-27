bca3058b40fbea6be953fd8f36c72d1b89c46006
Shivam Maheshwari <shivammaheshwari503@gmail.com>
2022-12-27 19:12:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from './components/login/Login'
+import Header from './components/header/Header';
+import Orders from './components/order/Orders';
+import Product from './components/products/Product';
+import Users from './components/users/Users';
+import {Routes, Route, Navigate} from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+const [isLogin, setIsLogin]  = useState(false)
+
+useEffect(() => {
+  axios
+    .get(`https://5fc1a1c9cb4d020016fe6b07.mockapi.io/api/v1/products`)
+    .then((Response) => {
+
+      localStorage.setItem("data", JSON.stringify(Response.data))
+     
+    });
+}, []);
+
+
+useEffect(() => {
+  axios
+      .get(`https://5fc1a1c9cb4d020016fe6b07.mockapi.io/api/v1/orders`)
+      .then((Response) => {
+          
+        localStorage.setItem("orderData", JSON.stringify(Response.data))
+      });
+}, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header onLogout={() => setIsLogin(false)} login={isLogin}/>
+      <Routes>
+      <Route path='/login' element={<Login onLogin={() =>  setIsLogin(true)}/>}></Route>
+      <Route path='/Orders' element={isLogin ? <Orders/> : <Navigate to="/login"/>}></Route>
+      <Route path='/Product' element={isLogin ? <Product/> : <Navigate to="/login"/>}></Route>
+      <Route path='/Users' element={isLogin ? <Users/> : <Navigate to="/login"/>}></Route>
+      </Routes>
     </div>
   );
 }
